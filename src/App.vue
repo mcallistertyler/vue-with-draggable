e6da3f4671d9d4a590af0e8d5a9cdeaf6bedbc18
Tyler McAllister <mcallistertyler95@gmail.com>
2021-09-02 19:23:57 +0200

    Add licence among other things

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<h1>Test</h1>
+	<draggable-thing></draggable-thing>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import DraggableThing from './components/DraggableThing.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+	DraggableThing,
   }
 }
 </script>
